9b27f0823aecb0f21a5aeaacad1b0c4029b54e72
amigler <amigler@calpoly.edu>
2024-04-15 20:18:30 +0000

    a1 ag

--- autograder/a1/ag.sh
+++ autograder/a1/ag.sh
@@ -1,7 +1,25 @@
-#! /bin/sh
+#! /bin/bash
 
-rm -f wc uniq ag_out
+rm -f wc357 uniq357 out_* test_*
 
-echo "(autograder tests not yet in place)"
+gcc word_count.c -o wc357 && echo "a aa\nbbbb\nc\n\naaa, 123\n" > test_wc1
+./wc357 test_wc1 > out_actual && wc test_wc1 | awk '{print $1,$2,$3}' > out_expected && diff out_actual out_expected
 
-exit 1
+./wc357 word_count.c > out_actual && wc word_count.c | awk '{print $1,$2,$3}' > out_expected && diff out_actual out_expected
+
+echo "\n" > test_wc2
+./wc357 test_wc2 > out_actual && wc test_wc2 | awk '{print $1,$2,$3}' > out_expected && diff out_actual out_expected
+
+
+gcc uniq.c -o uniq357
+
+echo -e "a\naa\naa\naa\nb\naa\na" > test_uniq1
+./uniq357 test_uniq1 > out_actual && uniq test_uniq1 > out_expected && diff out_actual out_expected
+
+
+./uniq357 uniq.c > out_actual && uniq uniq.c > out_expected && diff out_actual out_expected
+
+
+echo -e "a\na\na\na\n\naa\naa\na\na\n" > test_uniq3
+./uniq357 test_uniq3 > out_actual && uniq test_uniq3 > out_expected && diff out_actual out_expected
+
