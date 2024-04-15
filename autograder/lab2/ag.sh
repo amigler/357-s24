92b21bf740e8adf776bca6729634067c3d31d8b1
amigler <amigler@calpoly.edu>
2024-04-15 19:44:11 +0000

    ag

--- autograder/lab2/ag.sh
+++ autograder/lab2/ag.sh
@@ -9,15 +9,13 @@ echo -e "--task1--"
 gcc task1.c -o task1
 
 ./task1 here are -some command-line -arguments with some -dashes > ag_out
-
 echo -e "\ntask1a (expected / actual)"
 diff -y <(echo -e "-some\n-arguments\n-dashes") ag_out && echo "SUCCESS: task1 a"
 
 ./task1 no args with dashes > ag_out
-diff -yw <(echo "") ag_out && echo "SUCCESS: task1 a"
+diff -yw <(echo "") ag_out && echo "SUCCESS: task1 b"
 
 ./task1 -all -args -with -dashes > ag_out
-
 echo -e "\ntask1c (expected / actual)"
 diff -y <(echo -e "-all\n-args\n-with\n-dashes") ag_out && echo "SUCCESS: task1 c"
 
@@ -25,21 +23,23 @@ diff -y <(echo -e "-all\n-args\n-with\n-dashes") ag_out && echo "SUCCESS: task1 
 echo -e "--task2--"
 
 gcc task2.c -o task2
-echo -e "   \t a a aaa\t \n       abc " > test2a
-./task2 test2a > ag_out
 
+echo -e "   \t a a aaa\t \n       abc " > test2a #17
+./task2 test2a > ag_out
 echo -e "\ntask2a (expected / actual)"
 diff -yw <(echo -e "17") ag_out && echo "SUCCESS: task2 a"
 
-#17
+
+echo -e "   \t" > test2b  #5
+./task2 test2b > ag_out
+echo -e "\ntask2b (expected / actual)"
+diff -yw <(echo -e "5") ag_out && echo "SUCCESS: task2 b"
 
 
 touch empty_file
-
 ./task2 empty_file > ag_out
-
-echo -e "\ntask2b (expected / actual)"
-diff -yw <(echo "0") ag_out && echo "SUCCESS: task2 b"
+echo -e "\ntask2c (expected / actual)"
+diff -yw <(echo "0") ag_out && echo "SUCCESS: task2 c"
 
 #0
 
