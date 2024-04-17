d3dfbc2e4dc10c4ae52647b09620aeb9ab346653
amigler <amigler@calpoly.edu>
2024-04-17 23:40:11 +0000

    split test / actual commands

--- autograder/a1/ag.sh
+++ autograder/a1/ag.sh
@@ -8,7 +8,8 @@ rm -f wc357 uniq357 out_* test_*
 
 ((total++))
 gcc word_count.c -o wc357 && echo -e "a aa\nbbbb\nc\n\naaa, 123\n" > test_wc1
-timeout 10s ./wc357 test_wc1 > out_actual && wc test_wc1 | awk '{print $1,$2,$3}' > out_expected
+timeout 10s ./wc357 test_wc1 > out_actual
+wc test_wc1 | awk '{print $1,$2,$3}' > out_expected
 diff -y --suppress-common-lines out_actual out_expected
 if [ $? -ne 0 ]; then
     echo "ERROR: wc1 (actual / expected shown above)"
@@ -21,7 +22,8 @@ fi
 rm -f out_* test_*
 
 ((total++))
-timeout 10s ./wc357 word_count.c > out_actual && wc word_count.c | awk '{print $1,$2,$3}' > out_expected
+timeout 10s ./wc357 word_count.c > out_actual
+wc word_count.c | awk '{print $1,$2,$3}' > out_expected
 diff -y --suppress-common-lines out_actual out_expected
 if [ $? -ne 0 ]; then
     echo "ERROR: wc2 (actual / expected shown above)"
@@ -35,7 +37,8 @@ rm -f out_* test_*
 
 ((total++))
 echo -e "\n" > test_wc2
-timeout 10s ./wc357 test_wc2 > out_actual && wc test_wc2 | awk '{print $1,$2,$3}' > out_expected
+timeout 10s ./wc357 test_wc2 > out_actual
+wc test_wc2 | awk '{print $1,$2,$3}' > out_expected
 diff -y --suppress-common-lines out_actual out_expected
 if [ $? -ne 0 ]; then
     echo "ERROR: wc3 (actual / expected shown above)"
@@ -51,7 +54,8 @@ gcc uniq.c -o uniq357
 
 ((total++))
 echo -e "a\naa\naa\naa\nb\naa\na" > test_uniq1
-timeout 10s ./uniq357 test_uniq1 > out_actual && uniq test_uniq1 > out_expected
+timeout 10s ./uniq357 test_uniq1 > out_actual
+uniq test_uniq1 > out_expected
 diff -y --suppress-common-lines out_actual out_expected
 if [ $? -ne 0 ]; then
     echo "ERROR: uniq1 (actual / expected shown above)"
@@ -64,7 +68,8 @@ fi
 rm -f out_* test_*
 
 ((total++))
-timeout 10s ./uniq357 uniq.c > out_actual && uniq uniq.c > out_expected
+timeout 10s ./uniq357 uniq.c > out_actual
+uniq uniq.c > out_expected
 diff -y --suppress-common-lines out_actual out_expected
 if [ $? -ne 0 ]; then
     echo "ERROR: uniq2 (actual / expected shown above)"
@@ -78,7 +83,8 @@ rm -f out_* test_*
 
 ((total++))
 echo -e "a\na\na\na\n\naa\naa\na\na\n" > test_uniq3
-timeout 10s ./uniq357 test_uniq3 > out_actual && uniq test_uniq3 > out_expected
+timeout 10s ./uniq357 test_uniq3 > out_actual
+uniq test_uniq3 > out_expected
 diff -y --suppress-common-lines out_actual out_expected
 if [ $? -ne 0 ]; then
     echo "ERROR: uniq3 (actual / expected shown above)"
