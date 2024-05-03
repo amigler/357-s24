56ea4ae3ef7641ba6a817ae687de3c1cf7036ac6
amigler <amigler@calpoly.edu>
2024-05-03 19:19:35 +0000

    tree info

--- autograder/a3/ag.sh
+++ autograder/a3/ag.sh
@@ -2,7 +2,7 @@
 
 echo "Built-in tree command:"
 which tree
-tree -v
+tree --version
 
 wget -O a3_tests.tgz -q https://github.com/amigler/csc357-s23/blob/main/a3/a3_tests.tgz?raw=true && tar -xf a3_tests.tgz && chmod 755 a3_test.sh 
 
